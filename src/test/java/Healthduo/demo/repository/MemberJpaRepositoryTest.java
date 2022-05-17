e68b83a69dce77a7b42546218fcfe671e247fe4a
zontae <whdlsxo123@Naver.com>
2022-05-17 15:50:09 +0900

    test:중복 회원 검사

--- src/test/java/Healthduo/demo/repository/MemberJpaRepositoryTest.java
+++ src/test/java/Healthduo/demo/repository/MemberJpaRepositoryTest.java
@@ -3,20 +3,28 @@ package Healthduo.demo.repository;
 
 import Healthduo.demo.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
 class MemberJpaRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
     @Test
     void Membersave() {
         Member member = new Member();
@@ -28,14 +36,43 @@ class MemberJpaRepositoryTest {
         member.setMemberPnumber("010-2222-3333");
 
         System.out.println(member);
-        Member saveMember = memberRepository.save(member);
+        Member saveMember = memberRepository.saveMember(member);
 
 
         Member findmember = memberRepository.findById(saveMember.getMemberId()).get();
-        Assertions.assertThat(findmember.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(findmember.getMemberId()).isEqualTo(member.getMemberId());
     }
+
     @Test
-    void Login(){
+    @DisplayName("중복 아이디 회원 검증")
+    void overlapMemberSave() {
+
+        Member member1 = new Member();
+        member1.setMemberId("whdlsxo123");
+        member1.setMemberPassword("1234");
+        member1.setMemberSex("남");
+        member1.setMemberEmail("whdls123@naver.com");
+        member1.setMemberDate(LocalDate.now());
+        member1.setMemberPnumber("010-2222-3333");
+
+        em.persist(member1);
+        em.flush();
+        Member member2 = new Member();
+        member2.setMemberId("whdlsxo123");
+        member2.setMemberPassword("1234");
+        member2.setMemberSex("남");
+        member2.setMemberEmail("whdls123@naver.com");
+        member2.setMemberDate(LocalDate.now());
+        member2.setMemberPnumber("010-2222-3333");
+
+        assertThatThrownBy(() -> memberRepository.saveMember(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+
+    }
+
+    @Test
+    void Login() {
         Member member = new Member();
         member.setMemberId("whdlsxo123");
         member.setMemberPassword("1234");
@@ -47,9 +84,9 @@ class MemberJpaRepositoryTest {
         System.out.println(member);
         Member saveMember = memberRepository.save(member);
         Member findmember = memberRepository.findById(saveMember.getMemberId()).get();
-        
-        Assertions.assertThat(findmember.getMemberId()).isEqualTo("whdlsxo123");
-        Assertions.assertThat(findmember.getMemberPassword()).isEqualTo("1234");
+
+        assertThat(findmember.getMemberId()).isEqualTo("whdlsxo123");
+        assertThat(findmember.getMemberPassword()).isEqualTo("1234");
 
 
     }
