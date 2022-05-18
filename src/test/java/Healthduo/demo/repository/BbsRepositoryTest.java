cfb71ba0d40f67f5109047bbb48ec13b5f266082
zontae <whdlsxo123@Naver.com>
2022-05-18 20:11:54 +0900

    test:게시판 생성 ,업데이트

---
+++ src/test/java/Healthduo/demo/repository/BbsRepositoryTest.java
@@ -0,0 +1,54 @@
+package Healthduo.demo.repository;
+
+import Healthduo.demo.domain.Bbs;
+import Healthduo.demo.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+
+@SpringBootTest
+@Transactional
+class BbsRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    BbsRepository bbsRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void BbsSave(){
+        Member member = new Member();
+        member.setMemberId("아이디");
+        Bbs bbs = new Bbs("제목 Test", "내용 Test", "2022-05-18",1,"notice","secret",member);
+
+        bbsRepository.BbsSave(bbs);
+        Optional<Bbs> findbbs = bbsRepository.findById(1L);
+        Assertions.assertThat(findbbs.get().getBbsTitle()).isEqualTo("제목 Test");
+        Assertions.assertThat(findbbs.get().getBbsContent()).isEqualTo("내용 Test");
+
+    }
+    @Test
+    void BbsUpdate(){
+        Member member = new Member();
+        member.setMemberId("아이디");
+        Bbs bbs = new Bbs("제목 Test", "내용 Test", "2022-05-18",1,"notice","secret",member);
+
+        bbsRepository.BbsSave(bbs);
+
+        bbs.setBbsTitle("변경 Test");
+        bbs.setBbsContent("변경 Test");
+        Optional<Bbs> updatebbs = bbsRepository.findById(1L);
+        Assertions.assertThat(updatebbs.get().getBbsTitle()).isEqualTo("변경 Test");
+        Assertions.assertThat(updatebbs.get().getBbsContent()).isEqualTo("변경 Test");
+
+
+    }
+
+}
