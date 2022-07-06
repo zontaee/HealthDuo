a7d3e15f71450e6f58bdc5e33edf6dcba4972bdc
zontae <whdlsxo123@Naver.com>
2022-07-06 16:57:07 +0900

    feat: 회원가입 유효성 검사 추가

--- src/main/java/Healthduo/demo/WebConfig.java
+++ src/main/java/Healthduo/demo/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
-                .excludePathPatterns("/","/members/new","/member/checkinfomember","/login","/logout","/css/**", "/*.ico", "/error","/js/**","/checkinfomember");
+                .excludePathPatterns("/","/members/new","/member/checkinfomember","/login","/logout","/css/**", "/*.ico", "/error","/js/**","/checkinfomember","/duplicatedMember");
     }
 
 }
