4695f9fbbc39048b19a4aa742e92d0b7a576f786
zontae <whdlsxo123@Naver.com>
2022-07-19 00:54:47 +0900

    test :message test 추가

--- src/test/java/Healthduo/demo/web/ServiceMethodTest.java
+++ src/test/java/Healthduo/demo/web/ServiceMethodTest.java
@@ -1,21 +1,13 @@
 package Healthduo.demo.web;
 
-import Healthduo.demo.domain.Bbs;
-import Healthduo.demo.domain.Comment;
-import Healthduo.demo.domain.Member;
-import Healthduo.demo.domain.Region;
-import Healthduo.demo.repository.BbsRepository;
-import Healthduo.demo.repository.CommentRepository;
-import Healthduo.demo.repository.MemberRepository;
-import Healthduo.demo.repository.RegionRepository;
+import Healthduo.demo.domain.*;
+import Healthduo.demo.repository.*;
 import Healthduo.demo.service.CommentRestService;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -26,10 +18,10 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
+@Rollback(value = true)
 class ServiceMethodTest {
     @Autowired
     MemberRepository memberRepository;
@@ -45,6 +37,15 @@ class ServiceMethodTest {
     CommentRepository commentRepository;
     @Autowired
     CommentRestService commentRestService;
+    @Autowired
+    MessageSendRepository messageSendRepository;
+    @Autowired
+    MessageReceiveRepository messageReceiveRepository;
+/*
+    @AfterEach
+    public void afterEach(){
+    em.clear();
+    }*/
 
     @Test
     void bbsLnitialization() {
@@ -114,17 +115,17 @@ class ServiceMethodTest {
         assertThat(commentCnt).isEqualTo(0);
     }
 
-/*  트랜잭션 공부 후 다시
+
     @Test
     void saveComment() {
         Member member = new Member();
         member.setMemberId("아이디");
-        Bbs bbs = new Bbs("제목 Test", "내용 Test", "2022-05-18",1,true,false,member);
-        commentRestService.CommentSave("test",bbs,member);
-       Optional<Comment> comment = commentRepository.findById(1L);
+        Bbs bbs = new Bbs("제목 Test", "내용 Test", "2022-05-18", 1, true, false, member);
+        memberRepository.save(member);
+        commentRestService.CommentSave("test", bbs, "아이디");
+        Optional<Comment> comment = commentRepository.findById(1L);
         assertThat(comment.get().getContent()).isEqualTo("test");
     }
-*/
 
     @Test
     void incrementLevel() {
@@ -139,5 +140,59 @@ class ServiceMethodTest {
         Integer commentCnt = serviceMethod.fixedLevel(sliceChildInfo);
         assertThat(commentCnt).isEqualTo(0);
     }
+
+    @Test
+    void CheckError() {
+        String errorMessage = "";
+        //아이디 입력하지 않았거나 다를 경우
+        try {
+            Optional<Member> findReciveMemberId = Optional.empty();
+            serviceMethod.CheckError("title", "content", findReciveMemberId);
+        } catch (Exception e) {
+            errorMessage = e.getMessage();
+        }
+        assertThat(errorMessage).isEqualTo("받는 사람 아이디가 존재하지 않습니다.");
+        //쪽지 제목을 입력하지 않았을 경우
+        try {
+            Member member = new Member();
+            member.setMemberId("아이디");
+            Optional<Member> findReciveMemberId = Optional.of(member);
+            serviceMethod.CheckError("", "content", findReciveMemberId);
+        } catch (Exception e) {
+            errorMessage = e.getMessage();
+        }
+        assertThat(errorMessage).isEqualTo("쪽지 제목을 입력해주세요.");
+        //쪽지 내용을 입력하지 않았을 경우
+        try {
+            Member member = new Member();
+
+            Optional<Member> findReciveMemberId = Optional.of(member);
+            serviceMethod.CheckError("title", "", findReciveMemberId);
+        } catch (Exception e) {
+            errorMessage = e.getMessage();
+        }
+        assertThat(errorMessage).isEqualTo("쪽지 내용을 입력해주세요.");
+    }
+
+    @Test
+    void messageSendSave() {
+        Member member = new Member("sendId", "123", "남자", "whdlsxo123@naver.com", LocalDate.now(), "010-2323-4343");
+        Optional<Member> sendMemberInfo = Optional.of(member);
+        memberRepository.saveMember(member);
+        serviceMethod.messageSendSave("messageSendTitle", "receiveId", "content", "loginId", sendMemberInfo);
+        Optional<MessageSend> findMessageSend = messageSendRepository.findById(1L);
+        assertThat(findMessageSend.get().getMessageSendTitle()).isEqualTo("messageSendTitle");
+    }
+
+    @Test
+    void messageReceiveSave() {
+        Member member = new Member("sendId", "123", "남자", "whdlsxo123@naver.com", LocalDate.now(), "010-2323-4343");
+        Optional<Member> sendMemberInfo = Optional.of(member);
+        memberRepository.saveMember(member);
+        serviceMethod.messageReceivedSave("messageReceiveTitle", "receiveId", "content", "loginId", sendMemberInfo);
+        Optional<MessageReceive> findMessageReceice = messageReceiveRepository.findById(1L);
+        assertThat(findMessageReceice.get().getMessageReceiveTitle()).isEqualTo("messageReceiveTitle");
+    }
+
 
 }
